7dc988f42eefac8c788ac4b0f2ec8f32c80f8306
tanishqacodes <tanishqasharma797@gmail.com>
2022-11-06 21:48:22 +0530

    ingestion

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.exception import SensorException
+import os,sys
+from sensor.logger import logging
+# from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig
+from sensor.pipeline.training_pipeline import TrainPipeline
+
+# def test_exception():
+#     try:
+#         logging.info("We are dividing 1 by zero")
+#         # x=1/0
+    
+#     except Exception as e:
+#         raise SensorException(e,sys)
 
 if __name__ == '__main__':
-    mongodb_client = MongoDBClient()
-    print("collection name:",mongodb_client.database.list_collection_names())
+
+    training_pipeline = TrainPipeline()
+    training_pipeline.run_pipeline()
+    
+    # training_pipeline_config = TrainingPipelineConfig()
+    # data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+    # print(data_ingestion_config.__dict__)
+    # try:
+    #     test_exception()
+    # except Exception as e:
+    #     print(e)
+    # mongodb_client = MongoDBClient()
+    # print("collection name:",mongodb_client.database.list_collection_names())
--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -1,6 +1,8 @@
 import pymongo
 from sensor.constant.database import DATABASE_NAME
+from sensor.constant.env_variable import MONGODB_URL_KEY
 import certifi
+import os
 ca = certifi.where()
 
 class MongoDBClient:
@@ -9,10 +11,12 @@ class MongoDBClient:
     def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
             if MongoDBClient.client is None:
-                mongo_db_url ="mongodb+srv://Tanishqa:Tanishqa1810@cluster0.oevxeis.mongodb.net/?retryWrites=true&w=majority"
-                MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
-                self.client = MongoDBClient.client
-                self.database = self.client(database_name)
-                self.database_name = database_name
+                mongo_db_url = os.getenv(MONGODB_URL_KEY)
+            #     mongo_db_url ="mongodb+srv://Tanishqa:Tanishqa1810@cluster0.oevxeis.mongodb.net/?retryWrites=true&w=majority"
+            #     MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+            self.client = MongoDBClient.client
+            self.database = self.client[database_name]
+            self.database_name = database_name
         except Exception as e:
             raise e
+ 
